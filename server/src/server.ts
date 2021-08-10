bd02eb6e427e591124ff7d6f92966f05371c5f0d
danielpcampagna <danielprett@gmail.com>
2021-08-10 15:20:54 -0300

    feat: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,21 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
+import cors from 'cors';
 import { users } from './routers/users.routers';
 
 const app = express();
 const { PORT } = process.env;
 
-app.use("/api", users);
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
-app.get('/test', (req, res) => {
-  res.json(process.env)
-})
+app.use(function(req, res, next) {
+  const { headers } = req;
+  console.log(`Received a request from ${headers.origin}.`);
+  next();
+});
+
+app.use("/api", users);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
